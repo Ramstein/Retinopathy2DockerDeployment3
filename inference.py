738e702f9644e03965ebab31b954e4cd92b28b41
Ramstein <gerialworld@gmail.com>
2021-01-19 04:08:31 +0530

    committed by Ramstein

--- inference.py
+++ inference.py
@@ -23,7 +23,7 @@ from Retinopathy2.retinopathy.train_utils import report_checkpoint
 
 num_workers = multiprocessing.cpu_count()
 params = {}
-image_size = (512, 512)
+image_size = (1024, 1024)
 
 
 def image_with_name_in_dir(dirname, image_id):
